cd48fdeae27086ac00bb6d29f7ac49704f28cd5d
Tokitikoro <gabhaj37@gmail.com>
2026-08-11 11:52:22 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Dashboard from "@/components/Dashboard";
 import { getSurveys } from "@/lib/sheet";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const result = await getSurveys();
 
